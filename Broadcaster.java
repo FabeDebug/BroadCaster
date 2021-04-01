89ead9d91d963bc5338e941547642c907a2f224d
NilsGamer <78762946+NilsGamer@users.noreply.github.com>
2021-04-01 15:06:14 +0200

    Update Broadcaster.java

--- Broadcaster.java
+++ Broadcaster.java
@@ -1,6 +1,5 @@
-package de.nilsgamer.broadcaster.util;
 
-import de.nilsgamer.broadcaster.Main;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
